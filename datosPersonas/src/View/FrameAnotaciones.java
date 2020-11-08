41fa00e7236bc5c7876e88668b1715f480aeb2cd
Emanuel <emproducciones2257@gmail.com>
2020-11-08 10:18:34 -0300

    seguimiento con anotaciones

--- datosPersonas/src/View/FrameAnotaciones.java
+++ datosPersonas/src/View/FrameAnotaciones.java
@@ -10,6 +10,7 @@ import javax.swing.table.DefaultTableModel;
 import Control.controlAnotaciones;
 import Model.anotaciones;
 import java.awt.Color;
+import javax.swing.border.TitledBorder;
 
 public class FrameAnotaciones extends JFrame {
 
@@ -22,6 +23,11 @@ public class FrameAnotaciones extends JFrame {
 	private ArrayList<anotaciones> datos;
 	private ventanasAvisos avisos;
 	private controlAnotaciones contro;
+	private ButtonGroup buttonGroup;
+	private JRadioButton rdBtnBip,rdBtnAfip,rdBtnEmail;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	 
 	public FrameAnotaciones() {
@@ -29,7 +35,7 @@ public class FrameAnotaciones extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(FrameAnotaciones.class.getResource("/res/edit_property_40px.png")));
 		setTitle("Anotaciones");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 600, 400);
+		setBounds(100, 100, 590, 500);
 		contentPane = new JPanel();
 		contentPane.setBackground(SystemColor.text);
 		contentPane.setBorder(new LineBorder(SystemColor.windowText));
@@ -42,31 +48,88 @@ public class FrameAnotaciones extends JFrame {
 		
 		contro = new controlAnotaciones(this);
 		
+		buttonGroup = new ButtonGroup();
+		
 		btnBip = new JButton("");
 		btnBip.setBackground(Color.WHITE);
 		btnBip.setIcon(new ImageIcon(FrameAnotaciones.class.getResource("/res/bip.png")));
-		btnBip.setBounds(24, 11, 89, 89);
+		btnBip.setBounds(10, 11, 89, 89);
 		btnBip.addActionListener(contro);
 		contentPane.add(btnBip);
 		
-		btnAfip = new JButton("afip");
-		btnAfip.setBounds(240, 11, 89, 23);
+		btnAfip = new JButton("");
+		btnAfip.setIcon(new ImageIcon(FrameAnotaciones.class.getResource("/res/afip.png")));
+		btnAfip.setBounds(240, 11, 89, 89);
 		btnAfip.addActionListener(contro);
 		contentPane.add(btnAfip);
 		
-		btnEmail = new JButton("email");
-		btnEmail.setBounds(456, 11, 89, 23);
+		btnEmail = new JButton("");
+		btnEmail.setBackground(Color.WHITE);
+		btnEmail.setIcon(new ImageIcon(FrameAnotaciones.class.getResource("/res/icons8_email_64.png")));
+		btnEmail.setBounds(480, 11, 89, 89);
 		btnEmail.addActionListener(contro);
 		contentPane.add(btnEmail);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(24, 111, 537, 171);
+		scrollPane.setBounds(10, 111, 559, 171);
 		contentPane.add(scrollPane);
 					
 		tblDtos = new JTable();
 		tblDtos.setFillsViewportHeight(true);
 		
 		scrollPane.setViewportView(tblDtos);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Agregar - Editar", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(10, 293, 559, 143);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		rdBtnBip = new JRadioButton("Bip");
+		rdBtnBip.setBounds(6, 16, 109, 23);
+		panel.add(rdBtnBip);
+		buttonGroup.add(rdBtnBip);
+		
+		rdBtnAfip = new JRadioButton("Afip");
+		rdBtnAfip.setBounds(222, 16, 109, 23);
+		panel.add(rdBtnAfip);
+		buttonGroup.add(rdBtnAfip);
+		
+		rdBtnEmail = new JRadioButton("Email");
+		rdBtnEmail.setBounds(444, 16, 109, 23);
+		panel.add(rdBtnEmail);
+		buttonGroup.add(rdBtnEmail);
+		
+		JLabel lblNewLabel = new JLabel("Nombre");
+		lblNewLabel.setBounds(6, 56, 46, 14);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(6, 81, 86, 20);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Usuario");
+		lblNewLabel_1.setBounds(222, 56, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(222, 81, 86, 20);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Clave");
+		lblNewLabel_2.setBounds(444, 56, 46, 14);
+		panel.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(444, 81, 86, 20);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnRegistrar = new JButton("Guardar");
+		btnRegistrar.setBounds(464, 114, 89, 23);
+		panel.add(btnRegistrar);
 	}
 	
 	public JButton getBtnBip() {
